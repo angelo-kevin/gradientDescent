7886f7f67966527945caf3dd4e18692185ea08ee
Kevin Angelo <kevinn.angelo@gmail.com>
2020-02-26 20:01:13 +0700

    sklearn mlp done

--- sklMLP.py
+++ sklMLP.py
@@ -2,8 +2,14 @@ from sklearn.neural_network import MLPClassifier
 from sklearn import datasets
 
 def main():
+    layerNo = list(map(int, input("Jumlah node tiap layer, dipisahkan dengan space: ").split()))
+
     iris = datasets.load_iris()
-    clp = MLPClassifier()
+    clf = MLPClassifier(solver='sgd', hidden_layer_sizes=tuple(layerNo), activation='logistic', max_iter=1000)
+    clf.fit(iris.data, iris.target)
+
+    for i in clf.coefs_:
+        print(i) # returns the weight of the network
 
 if __name__ == "__main__":
     main()
