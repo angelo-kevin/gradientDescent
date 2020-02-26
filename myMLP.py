ee8c3fa4fa27440cdfcb939b13bf7ca9264e79c1
Kevin Angelo <kevinn.angelo@gmail.com>
2020-02-26 16:29:24 +0700

    batching and sigmoid

---
+++ myMLP.py
@@ -0,0 +1,23 @@
+import numpy as np
+from sklearn import datasets
+
+batchUnit = 10 # batch size
+
+def sigmoid(x):
+    return 1/(1 + np.exp(-x))
+
+def batching(iris, counter):
+    data = iris.data[counter*batchUnit:(counter*batchUnit)+batchUnit]
+    target = iris.target[counter*batchUnit:(counter*batchUnit)+batchUnit]
+    
+    return data, target
+
+def main():
+    iris = datasets.load_iris()
+    # use batching() in a loop, feeding the batched datasets to the model
+    for i in range(len(iris.data)):
+        dataTemp, targetTemp = batching(iris, i)
+        # manipulate dataTemp and targetTemp here
+
+if __name__ == "__main__":
+    main()
