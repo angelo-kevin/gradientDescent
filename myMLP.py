cb6bf153f34146ba2cf23cbe444d42c956cc28cc
Juro <jsutantra@gmail.com>
2020-02-27 18:48:03 +0700

    backward phase ver 1

--- myMLP.py
+++ myMLP.py
@@ -1,7 +1,39 @@
 import numpy as np
+import copy
 from sklearn import datasets
 
 batchUnit = 10 # batch size
+
+def count_sigma(MLP_dictionary, i_layer,node, delta_matrix) :
+    sum = 0
+    for keys in MLP_dictionary.keys() :
+        if keys[0] == i_layer+1 and keys[1]==node :
+            print(keys)
+            sum = sum + MLP_dictionary[keys] * delta_matrix[i_layer+1][keys[2]]
+    return sum
+
+def make_delta_matrix(feed_forward_output_matrix, target_data, MLP_dictionary):
+    result_matrix = copy.deepcopy(feed_forward_output_matrix)
+    for row in result_matrix :
+        for i in range(0,len(row)) :
+            row[i] = 0
+    result_matrix[len(result_matrix)-1][0] = feed_forward_output_matrix[len(result_matrix)-1][0]*(1-feed_forward_output_matrix[len(result_matrix)-1][0]) * (target_data - feed_forward_output_matrix[len(result_matrix)-1][0]) 
+    for i in range(len(feed_forward_output_matrix)-2,-1,-1 ) :
+        for j in range(0,len(feed_forward_output_matrix[i])):
+            print(i,j)
+            result_matrix[i][j] = feed_forward_output_matrix[i][j]*(1-feed_forward_output_matrix[i][j]) * count_sigma(MLP_dictionary,i,j,result_matrix)
+    return result_matrix
+
+def backward_phase (feed_forward_output_matrix, MLP_dictionary, n_layer, x_data, target_data , delta_weight_dictionary) :
+    delta_matrix = make_delta_matrix(feed_forward_output_matrix, target_data, MLP_dictionary)
+    for k in range (n_layer-1, 0, -1) :
+        for keys in MLP_dictionary.keys():
+            if k == keys[0]  :
+                delta_weight_dictionary[keys] += delta_weight_dictionary[keys] +  0.1*delta_matrix[k][keys[2]] * feed_forward_output_matrix[k-1][keys[1]]
+    for i in range(0,len(x_data)) :
+        for keys in MLP_dictionary.keys():
+            if keys[0] == 0 and keys[1]==i:
+                delta_weight_dictionary[keys] += delta_weight_dictionary[keys] +  0.1*delta_matrix[0][keys[2]] *x_data[i]
 
 # network weights in dict data structure
 def initWeight(layerNo, inputlayer = 4, outputlayer = 1):
@@ -36,6 +68,6 @@ def main():
     weight = initWeight(layerNo)
     iris = datasets.load_iris()
     train(iris)
-
+    
 if __name__ == "__main__":
     main()
