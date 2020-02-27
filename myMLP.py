58190231944933a0366e305362926eb0b4cb02ce
tasyald <13517141@std.stei.itb.ac.id>
2020-02-27 14:13:45 +0700

    Forward done

--- myMLP.py
+++ myMLP.py
@@ -11,7 +11,7 @@ def initWeight(layerNo, inputlayer = 4, outputlayer = 1):
     for i in range(len(layerNo)-1):
         for j in range(layerNo[i]):
             for k in range(layerNo[i+1]):
-                weight[(i,j,k)] = 0
+                weight[(i,j,k)] = 1
     
     return weight
 
@@ -22,20 +22,47 @@ def batching(iris, counter):
     startIdx = counter*batchUnit
     data = iris.data[startIdx:(startIdx)+batchUnit]
     target = iris.target[startIdx:(startIdx)+batchUnit]
+    print(data)
     
     return data, target
 
-def train(iris):
+def forward(record, weight, layerNo, inputlayer = 4):
+    # Initialize output matrix
+    output = []
+    for i in range(len(layerNo) - 2):
+        output.append([])
+        for k in range(layerNo[i + 1]):
+            output[i].append(0)
+    output.append([0])  # Output layer
+
+    # Calculate out
+    for i in range(len(layerNo) - 1):
+        if (i == 0):
+            for k in range(int(layerNo[i + 1])):
+                for j in range(len(record)):
+                    output[i][k] += weight[(i, j, k)] * record[j]
+                output[i][k] = sigmoid(output[i][k])
+        else:
+            for k in range(int(layerNo[i + 1])):
+                for j in range(int(layerNo[i])):
+                    output[i][k] += weight[(i, j, k)] * output[i - 1][k - 1]
+                output[i][k] = sigmoid(output[i][k])
+    
+    return(output)
+
+def train(iris, weight, layerNo):
     # use batching() in a loop, feeding the batched datasets to the model
     for i in range(len(iris.data)//batchUnit):
         dataTemp, targetTemp = batching(iris, i)
         # manipulate dataTemp and targetTemp here
+        for record in dataTemp:
+            forward(record, weight, layerNo)
 
 def main():
     layerNo = list(map(int, input("Jumlah node tiap layer, dipisahkan dengan space: ").split()))
     weight = initWeight(layerNo)
     iris = datasets.load_iris()
-    train(iris)
+    train(iris, weight, layerNo)
 
 if __name__ == "__main__":
     main()
